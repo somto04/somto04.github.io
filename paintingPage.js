9f9ad4b0eed9a22a3f54cee0d87e2bbef00cec0e
somto04 <s.azubuike1@lancaster.ac.uk>
2026-08-07 22:41:37 -0500

    starting script for painting details

--- paintingPage.js
+++ paintingPage.js
@@ -1,3 +1,19 @@
 function openPainting(id) {
     window.location.href = "painting.html?id=" + id;
 }
+
+const paintings = {
+    swirlyWirly: {
+        title: "Swirly Wirly",
+        image: "artworks/swirlywirly.jpeg",
+        desc: "yabba dabba doo"
+    },
+}
+
+const params = URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const painting = paintings[id];
+document.getElementById("painting-title").textContent = painting.title;
+document.getElementById("painting-image").src = painting.image;
+document.getElementById("painting-desc").textContent = painting.desc;
